a34e409491f8270c0d852628ede908a6eabde21d
Guilherme Britto <guilhermefragab@gmail.com>
2023-07-28 21:20:55 -0300

    feat: front login

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import { AuthProvider } from "./providers/AuthProvider";
+import { RoutesMain } from "./routes";
+import GlobalStyles from "./styles/GlobalStyles";
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <AuthProvider>
+        <RoutesMain />
+      </AuthProvider>
+    </>
+  );
+};
